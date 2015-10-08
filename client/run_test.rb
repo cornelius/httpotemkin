e1cd9d78558a536644fe7fa2c8a61b4621d45b29
Cornelius Schumacher <schumacher@kde.org>
2015-10-09 01:12:11 +0200

    Make fake servers work for osc and gem

--- client/run_test.rb
+++ client/run_test.rb
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl --cacert /certificates/rubygems.crt https://api.rubygems.org -s
-curl --cacert /certificates/obs.crt https://api.opensuse.org -s
+curl https://rubygems.org -s
+curl https://api.rubygems.org -s
+curl https://api.opensuse.org -s
--- run_test.rb
+++ run_test.rb
@@ -9,12 +9,18 @@ def run(cmd)
 end
 
 run("docker run --name=rubygems -d rubygems")
+run("docker run --name=api.rubygems -d api.rubygems")
 run("docker run --name=obs -d obs")
 sleep 3
 
-run("docker run --link rubygems:api.rubygems.org --link obs:api.opensuse.org client")
+run("docker run --link rubygems:rubygems.org --link api.rubygems:api.rubygems.org --link obs:api.opensuse.org client")
 
-run("docker rm -f rubygems")
-run("docker rm -f obs")
+run("docker kill rubygems")
+run("docker kill api.rubygems")
+run("docker kill obs")
+
+run("docker rm rubygems")
+run("docker rm api.rubygems")
+run("docker rm obs")
 
 puts "Success."
