8b5f5d9ac195460db7e3803716df0e42a00133c6
Cornelius Schumacher <schumacher@kde.org>
2015-10-11 00:51:42 +0200

    Check test results

--- run_test.rb
+++ run_test.rb
@@ -14,16 +14,36 @@ servers = {
   "obs" => "api.opensuse.org"
 }
 
+expected_output = File.read("expected_output")
+
 servers.keys.each do |name|
   run("docker run --name=#{name} -d #{name}")
 end
 sleep 3
 
 links = servers.map { |name, server| "--link #{name}:#{server}" }.join(" ")
-run("docker run #{links} client")
+cmd = "docker run #{links} client"
+puts cmd
+
+puts "--- Start Test ---"
+actual_output = `#{cmd}`
+puts actual_output
+puts "---- End Test ----"
 
 servers.keys.each do |name|
   run("docker rm -f #{name}")
 end
 
-puts "Success."
+puts
+if actual_output == expected_output
+  puts "Success."
+else
+  puts "Failed."
+  puts
+  puts "Expected output:"
+  puts expected_output
+  puts
+  puts "Actual output:"
+  puts actual_output
+  exit 1
+end
