052955f06a21f224a1c8a899d0beb2b3d2cc7a2b
Cornelius Schumacher <schumacher@kde.org>
2015-12-03 23:21:04 +0100

    Make servers behave correctly for `gem push`

--- containers/api.rubygems/secure_server.rb
+++ containers/api.rubygems/secure_server.rb
@@ -11,3 +11,11 @@ configure_sinatra
 get '/' do
   "I'm the secure api.rubygems server\n"
 end
+
+get '/latest_specs.4.8.gz' do
+  File.read(File.expand_path("../latest_specs.4.8.gz", __FILE__))
+end
+
+get '/quick/Marshal.4.8/rubygems-update-2.5.0.gemspec.rz' do
+  File.read(File.expand_path("../rubygems-update-2.5.0.gemspec.rz", __FILE__))
+end
--- containers/rubygems/secure_server.rb
+++ containers/rubygems/secure_server.rb
@@ -11,3 +11,29 @@ configure_sinatra
 get '/' do
   "I'm the secure rubygems server\n"
 end
+
+post '/api/v1/gems' do
+  "Successfully registered gem: red_herring (0.0.2)"
+end
+
+get '/api/v1/versions/red_herring.json' do
+  json = <<EOT
+[{
+  "authors": "Cornelius Schumacher",
+  "built_at": "2015-06-20T00:00:00.000Z",
+  "created_at": "2015-06-20T06:57:40.675Z",
+  "description": "This is a red herring.",
+  "downloads_count": 371,
+  "metadata": {},
+  "number": "0.0.1",
+  "summary": "Red herring",
+  "platform": "ruby",
+  "ruby_version": "\u003e= 0",
+  "prerelease": false,
+  "licenses": ["MIT"],
+  "requirements": [],
+  "sha": "8afa714a16b34800a3f4e7aefcab871cf98aca33d02758e9c9d658cbf4bd740b"
+}]
+EOT
+  json
+end
